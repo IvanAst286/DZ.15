e6d3a91da94d385aa61c17b3193895f614b92677
ivast <ivast@DESKTOP-31KVF2T>
2025-11-24 01:04:09 +0500

    wfwf

--- DZ.15/main.cpp
+++ DZ.15/main.cpp
@@ -1,210 +1,189 @@
 #include <iostream>
 #include <ctime>
-#include <cstdlib>
 #include <iomanip>
 #include <Windows.h>
 
 
-const int ROWS = 18;
-const int COLS = 36;
+const int ROWS = 18; 
+const int COLS = 36; 
 
-void randomFill(int train[][COLS])
-{
-    srand(time(nullptr));
+int trainTickets[ROWS][COLS];
 
-    for (int i = 0; i < ROWS; ++i)
+void randomFill() {
+    srand(static_cast<unsigned>(time(nullptr)));
+    for (int i = 0; i < ROWS; ++i) 
     {
-        for (int j = 0; j < COLS; ++j)
+        for (int j = 0; j < COLS; ++j) 
         {
-            train[i][j] = rand() % 2;
+            trainTickets[i][j] = rand() % 2; 
         }
     }
 }
 
-void emptyFill(int train[][COLS])
+
+void emptyFill() 
 {
-    for (int i = 0; i < ROWS; ++i)
+    for (int i = 0; i < ROWS; ++i) 
     {
-        for (int j = 0; j < COLS; ++j)
+        for (int j = 0; j < COLS; ++j) 
         {
-            train[i][j] = 0;
+            trainTickets[i][j] = 0; 
         }
     }
 }
 
-void printTrain(const int train[][COLS], const char* mode)
+
+void printTrain() 
 {
-    std::cout << "\nТекущее состояние поездов (" << mode << ") :\n";
-    for (int i = 0; i < ROWS; ++i) {
+    std::cout << "Текущее состояние билетов:\n";
+    for (int i = 0; i < ROWS; ++i) 
+    {
         std::cout << "Вагон №" << i + 1 << ": ";
-
-        for (int j = 0; j < COLS; ++j)
+        for (int j = 0; j < COLS; ++j) 
         {
-            if (train[i][j] == 0)
-            {
-                std::cout << "0";
-            }
-            else
-            {
-                std::cout << "1";
-            }
-
-            if ((j + 1) % 6 == 0 && j != COLS - 1)
-            {
-                std::cout << " ";
-            }
+            std::cout << trainTickets[i][j] << " ";
         }
         std::cout << "\n";
     }
 }
 
-void changeSeatState(int train[][COLS])
+
+void changeSeatState() 
 {
-    int wagonNumber, seatNumber;
-    bool validInput = false;
+    int wagon, seat;
+    std::cout << "Введите номер вагона (от 1 до " << ROWS << "): ";
+    std::cin >> wagon;
+    std::cout << "Введите номер места (от 1 до " << COLS << "): ";
+    std::cin >> seat;
 
-    while (!validInput)
+    --wagon;
+    --seat;
+
+    if (wagon >= 0 && wagon < ROWS && seat >= 0 && seat < COLS) 
     {
-        std::cout << "Выберите вагон (от 1 до " << ROWS << "): ";
-        std::cin >> wagonNumber;
-        --wagonNumber;
-
-        if (wagonNumber >= 0 && wagonNumber < ROWS)
+        if (trainTickets[wagon][seat] == 0) 
         {
-            std::cout << "Выберите место (от 1 до " << COLS << "): ";
-            std::cin >> seatNumber;
-            --seatNumber;
-
-            if (seatNumber >= 0 && seatNumber < COLS)
-            {
-                validInput = true;
-
-                if (train[wagonNumber][seatNumber] == 0)
-                {
-                    train[wagonNumber][seatNumber] = 1;
-                    std::cout << "Место успешно забронировано.\n";
-                }
-                else
-                {
-                    train[wagonNumber][seatNumber] = 0;
-                    std::cout << "Место освобождено.\n";
-                }
-            }
-            else
-            {
-                std::cout << "Неверный номер места!\n";
-            }
+            trainTickets[wagon][seat] = 1;
+            std::cout << "Место успешно забронировано.\n";
         }
-        else
+        else 
         {
-            std::cout << "Неверный номер вагона!\n";
+            trainTickets[wagon][seat] = 0; 
+            std::cout << "Место успешно освобождено.\n";
         }
+    }
+    else 
+    {
+        std::cout << "Неверный номер вагона или места.\n";
     }
 }
 
-int countFreeSeatsInWagon(const int train[][COLS], int wagonIndex)
+int countFreeSeatsInWagon(int wagon) 
 {
-    int freeSeats = 0;
-    for (int j = 0; j < COLS; ++j)
+    int count = 0;
+    for (int j = 0; j < COLS; ++j) 
     {
-        if (train[wagonIndex][j] == 0)
+        if (trainTickets[wagon][j] == 0) 
         {
-            ++freeSeats;
+            ++count;
         }
     }
-    return freeSeats;
+    return count;
 }
 
-int countTotalFreeSeats(const int train[][COLS])
+int countTotalFreeSeats() 
 {
-    int totalFreeSeats = 0;
-    for (int i = 0; i < ROWS; ++i)
+    int totalFree = 0;
+    for (int i = 0; i < ROWS; ++i) 
     {
-        totalFreeSeats += countFreeSeatsInWagon(train, i);
+        totalFree += countFreeSeatsInWagon(i);
     }
-    return totalFreeSeats;
+    return totalFree;
 }
 
-int main()
+int main() 
 {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     srand(time(NULL));
-    setlocale(LC_ALL, "");
 
-    int train[ROWS][COLS];
-
-    int choice;
-    std::cout << "\nВыберите способ заполнения:\n"
-        << "1. Рандомное заполнение\n"
-        << "2. Все места свободны\n"
-        << "Ваш выбор: ";
-    std::cin >> choice;
-
-    if (choice == 1)
-    {
-        randomFill(train);
-    }
-    else if (choice == 2)
-    {
-        emptyFill(train);
-    }
-    else
-    {
-        std::cerr << "Ошибка: некорректный выбор заполнения.\n";
-        exit(EXIT_FAILURE);
-    }
-
-    int actionChoice;
     while (true)
     {
-        std::cout << "\nЧто хотите сделать?\n"
-            << "1. Посмотреть текущее состояние состава\n"
-            << "2. Изменить состояние места (забронировать/освободить)\n"
-            << "3. Узнать количество свободных мест в вагоне\n"
-            << "4. Узнать общее количество свободных мест во всех вагонах\n"
-            << "5. Завершить работу\n"
+        int fillMode;
+        std::cout << "Выберите режим заполнения билетов:\n"
+            << "1. Случайное заполнение\n"
+            << "2. Все места свободны\n"
             << "Ваш выбор: ";
-        std::cin >> actionChoice;
+        std::cin >> fillMode;
 
-        if (actionChoice == 1)
+        if (fillMode == 1) 
         {
-            printTrain(train, (choice == 1 ? "случайное заполнение" : "все места свободны"));
+            randomFill();
         }
-        else if (actionChoice == 2)
+        else if (fillMode == 2) 
         {
-            changeSeatState(train);
+            emptyFill(); 
         }
-        else if (actionChoice == 3)
+        else 
         {
-            int wagonNumber;
-            std::cout << "Для какого вагона посчитать свободные места (от 1 до " << ROWS << ")? ";
-            std::cin >> wagonNumber;
+            std::cerr << "Ошибка: неверный выбор режима заполнения.\n";
+            continue;
+        }
 
-            if (wagonNumber > 0 && wagonNumber <= ROWS)
+        while (true) 
+        {
+            int action;
+            std::cout << "\nВыберите действие:\n"
+                << "1. Показать текущее состояние билетов\n"
+                << "2. Изменить состояние места (забронировать/освободить)\n"
+                << "3. Узнать количество свободных мест в вагоне\n"
+                << "4. Узнать общее количество свободных мест\n"
+                << "5. Закрыть программу\n"
+                << "Ваш выбор: ";
+            std::cin >> action;
+
+            if (action == 1) 
             {
-                int freeSeats = countFreeSeatsInWagon(train, wagonNumber - 1);
-                std::cout << "Свободных мест в вагоне №" << wagonNumber << ": " << freeSeats << "\n";
+                printTrain(); 
             }
-            else
+            else if (action == 2) 
             {
-                std::cout << "Некорректный номер вагона." << "\n";
+                changeSeatState(); 
             }
-        }
-        else if (actionChoice == 4)
-        {
-            int totalFreeSeats = countTotalFreeSeats(train);
-            std::cout << "Общее количество свободных мест во всех вагонах: " << totalFreeSeats << "\n";
-        }
-        else if (actionChoice == 5)
-        {
-            break;
-        }
-        else
-        {
-            std::cout << "Ошибка: неверный ввод команды.\n";
+            else if (action == 3) 
+            {
+                int wagonNumber;
+                std::cout << "Введите номер вагона (от 1 до " << ROWS << "): ";
+                std::cin >> wagonNumber;
+                --wagonNumber; 
+                if (wagonNumber >= 0 && wagonNumber < ROWS) 
+                {
+                    std::cout << "Свободных мест в вагоне №" << wagonNumber + 1 << ": " << countFreeSeatsInWagon(wagonNumber) << "\n";
+                }
+                else 
+                {
+                    std::cout << "Неверный номер вагона.\n";
+                    continue;
+                }
+            }
+            else if (action == 4) 
+            {
+                std::cout << "Общее количество свободных мест: " << countTotalFreeSeats() << "\n";
+            }
+            else if (action == 5) 
+            {
+                std::cout << "Завершают работу программы.\n";
+                break;
+            }
+            else 
+            {
+                std::cout << "Ошибка: неправильно выбрано действие.\n";
+                continue;
+            }
         }
     }
+
+   
 
     return 0;
 }
